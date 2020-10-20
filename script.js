1633032e2423aa66cedf65bf98eee95e75d957fe
rizkybaihaqy <rizkybaihaqy123@gmail.com>
2020-10-20 23:02:07 +0700

    commit pertama kali

---
+++ script.js
@@ -0,0 +1,38 @@
+function nama()
+{
+    if(document.forms["formContact"]["name"].value == "")
+    {
+        alert("Please enter your name.");
+            return false;
+    }
+}
+
+function email()
+{
+    if(document.forms["formContact"]["email"].value == "")
+    {
+        alert("Please enter your email.");
+        return false;
+    }
+}
+
+function message()
+{
+    if(document.forms["formContact"]["message"].value == ""){
+        alert("Please enter your message.");
+        return false;
+    }
+}
+
+function validateForm(){
+    nama();
+    email();
+    message();
+}
+
+$(function () {
+    $(document).scroll(function() {
+        var $nav = $("#navbarMain");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height() );
+    })
+})
